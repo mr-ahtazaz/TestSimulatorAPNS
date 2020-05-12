2c5d56e68e73ac12e6773a516dddd0a87b8db09f
Ahtazaz Khan <mr.ahtazaz@gmail.com>
2020-05-12 12:01:18 +0500

    Initial Commit

---
+++ TestSimulatorAPNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSimulatorAPNS
+//
+//  Created by Ahtazaz Khan on 5/12/20.
+//  Copyright © 2020 Ahtazaz Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
